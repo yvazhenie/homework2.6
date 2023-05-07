04e09185515b80180b45d9046c34872fb0e521a5
Makarov <infernochetac31@gmail.com>
2023-05-07 15:36:56 +0300

    start

---
+++ src/test/java/com/makarov/homework2/PR/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarov.homework2.PR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
